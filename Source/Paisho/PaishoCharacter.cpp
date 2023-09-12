99973cfc1386ecfa140539716eec365f48a3302e
haasg <griffin.haas1@gmail.com>
2023-09-12 10:50:57 -0700

    rotate on sprite for now

--- Source/Paisho/PaishoCharacter.cpp
+++ Source/Paisho/PaishoCharacter.cpp
@@ -1,11 +1,26 @@
 #include "PaishoCharacter.h"
 
+#include "Camera/CameraComponent.h"
 #include "Framework/PaishoPlayerState.h"
+#include "GameFramework/SpringArmComponent.h"
 
 APaishoCharacter::APaishoCharacter()
 {
 	bReplicates = true;
 	//SetReplicatedMovement(true);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->bDoCollisionTest = false;
+	//SpringArm->bUsePawnControlRotation = true;
+	// const FRotator SpringArmRotation = FRotator(-90.f, 0.f, 270.f);
+	// SpringArm->SetWorldRotation(SpringArmRotation);
+	SpringArm->TargetArmLength = 1920.0f;
+	
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
 }
 
 void APaishoCharacter::Tick(float DeltaSeconds)
--- Source/Paisho/PaishoCharacter.h
+++ Source/Paisho/PaishoCharacter.h
@@ -12,5 +12,12 @@ class PAISHO_API APaishoCharacter : public APaperZDCharacter
 	APaishoCharacter();
 
 	virtual void Tick(float DeltaSeconds) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	class UCameraComponent* Camera;
 	
 };
