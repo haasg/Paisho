58015cab551af10de25da32b551ba732e3a85076
haasg <griffin.haas1@gmail.com>
2023-09-13 08:29:41 -0700

    add weapon class

---
+++ Source/Paisho/Weapons/Weapon.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class AWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	AWeapon() {}
+
+	virtual void Fire();
+	
+};
