5158fe4c1f32e98c9049732556654022d25fa98c
haasg <griffin.haas1@gmail.com>
2023-09-17 20:58:36 -0700

    checkpoint

--- Source/Paisho/Weapons/Projectile.h
+++ Source/Paisho/Weapons/Projectile.h
@@ -39,6 +39,12 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UProjectileMovementComponent* MovementComponent;
 
+	UPROPERTY(VisibleAnywhere)
+	float DamagePerHit;
+
+	UPROPERTY(VisibleAnywhere)
+	float CurrentPenetration;
+
 private:
 	UFUNCTION()
 	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
