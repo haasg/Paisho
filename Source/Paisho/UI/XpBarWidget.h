e8f5036e9fb268eb834e55a0e1a443c54a7746c2
haasg <griffin.haas1@gmail.com>
2023-09-20 22:11:17 -0700

    make the c++ xp bar widget and reparent the bp

---
+++ Source/Paisho/UI/XpBarWidget.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "XpBarWidget.generated.h"
+
+class UTextBlock;
+class UProgressBar;
+
+UCLASS()
+class UXpBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	TObjectPtr<UProgressBar> XpBar;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	TObjectPtr<UTextBlock> XpText;
+	
+};
