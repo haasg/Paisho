60d0dbb093bda6095cd5b323df110e584621a78f
haasg <griffin.haas1@gmail.com>
2023-10-09 21:45:43 -0700

    get fire blast working

--- Source/Paisho/Weapons/FireBlast.cpp
+++ Source/Paisho/Weapons/FireBlast.cpp
@@ -1,14 +1,30 @@
 #include "FireBlast.h"
 
 #include "Projectile.h"
+#include "Paisho/Character/PaishoVillain.h"
+#include "Paisho/Framework/PaishoGameState.h"
 #include "Paisho/Data/WeaponData.h"
+#include "Paisho/Util/DebugUtil.h"
 
 void AFireBlast::Fire()
 {
-	AProjectile* Projectile = GetWorld()->SpawnActor<AProjectile>(AProjectile::StaticClass());
-	Projectile->SetLocation(GetActorLocation());
-	Projectile->SetSprite(WeaponData->GetProjectileSprite());
-	Projectile->SetSpeed(WeaponData->GetProjectileSpeed());
-	Projectile->SetDirection(FVector(1.f, 0.f, 0.f));
+	if(const TObjectPtr<APaishoGameState> GameState = GetWorld()->GetGameState<APaishoGameState>())
+	{
+		const FVector MyLocation = GetActorLocation();
+		if(const TObjectPtr<APaishoVillain> RandomVillain = GameState->GetRandomVillain())
+		{
+			AProjectile* Projectile = GetWorld()->SpawnActor<AProjectile>(AProjectile::StaticClass());
+			Projectile->SetLocation(MyLocation);
+			Projectile->SetSprite(WeaponData->GetProjectileSprite());
+			
+			Projectile->SetSpeed(WeaponData->GetProjectileSpeed());
+			const FVector RandomVillainLocation = RandomVillain->GetActorLocation();
+			const FVector UnitVectorToRandomVillain = (RandomVillainLocation - MyLocation).GetSafeNormal();
+			Projectile->SetDirection(UnitVectorToRandomVillain);
+			const FRotator AngleToRandomVillain = UnitVectorToRandomVillain.Rotation();
+			const FRotator SpriteRotation = FRotator(180, 180, 0);
+			Projectile->SetRotation(AngleToRandomVillain + SpriteRotation);
+		}
+	} ELSE_ERROR("FireBlast::Fire with nullptr GameState")
 }
 
