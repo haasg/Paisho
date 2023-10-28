763dfd3e1c67fc6f611ec30d59e30ec4358fe731
haasg <griffin.haas1@gmail.com>
2023-10-28 11:29:59 -0700

    create elements ui display

---
+++ Source/Paisho/UI/ElementsWidget.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "ElementsWidget.generated.h"
+
+class UCommonTextBlock;
+
+UCLASS()
+class PAISHO_API UElementsWidget : public UCommonUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> WaterLevelText;
+
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> FireLevelText;
+
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> NatureLevelText;
+
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> EarthLevelText;
+
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> LightLevelText;
+	
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> VoidLevelText;
+};
