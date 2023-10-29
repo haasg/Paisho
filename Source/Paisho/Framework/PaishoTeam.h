77e6c1548b5131606e76cf41e30f32a14381f3a2
haasg <griffin.haas1@gmail.com>
2023-10-29 10:13:25 -0700

    reorganize team

--- Source/Paisho/Framework/PaishoTeam.h
+++ Source/Paisho/Framework/PaishoTeam.h
@@ -17,7 +17,11 @@ public:
 	APaishoTeam();
 	virtual void Tick(float DeltaTime) override;
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+protected:
+	virtual void BeginPlay() override;
 
+
+	
 	/* Team */
 protected:
 	UPROPERTY(VisibleAnywhere)
@@ -25,32 +29,31 @@ protected:
 
 
 
+	/* Players */
+public:
 	void Join(TObjectPtr<APaishoPlayerController> PlayerController);
-	//void BindUIToPlayer(TObjectPtr<APaishoPlayerController> PlayerController);
-	void CollectXp(int32 Amount);
-
 	TArray<TObjectPtr<APaishoPlayerController>> GetPlayers() const { return Players; }
-
 protected:
-	virtual void BeginPlay() override;
-	void PollForUnpause();
-
-	UFUNCTION()
-	void HandleLevelUp(int Level);
-	
-	
-
-private:
-
-	
-
 	UFUNCTION()
 	void OnRep_Players();
 
 	UPROPERTY(ReplicatedUsing = OnRep_Players, VisibleAnywhere)
 	TArray<TObjectPtr<APaishoPlayerController>> Players;
+
+
+	
+	/* Xp */
+public:
+	void CollectXp(int32 Amount);
+	//void BindUIToPlayer(TObjectPtr<APaishoPlayerController> PlayerController);
+protected:
+	void PollForUnpause();
+
+	UFUNCTION()
+	void HandleLevelUp(int Level);
 	
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UXpComponent> XpComponent;
-	
+
+
 };
